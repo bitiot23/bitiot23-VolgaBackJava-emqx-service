3b59f506f0ed9a0910304676b3fc230061288426
Cesar Munoz <cesarmu@hotmail.com>
2024-09-29 19:58:42 -0500

    Se agrego el paquete services y adentro la clase CameraDataClass con los campos que se esperan del JSON que se obtiene del topico de las camaras

---
+++ src/main/java/com/bitiot/volga3/emqx_to_rabbit/app/service/CameraDataClass.java
@@ -0,0 +1,34 @@
+package com.bitiot.volga3.emqx_to_rabbit.app.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignora campos desconocidos
+public class CameraDataClass {
+
+    @JsonProperty("NameCamera")
+    private String nameCamera;
+
+    @JsonProperty("ReportTime")
+    private String reportTime;
+
+    @JsonProperty("idBranchOffice")
+    private String idBranchOffice;
+
+    @JsonProperty("Entrada")
+    private String entrada;
+
+    @JsonProperty("Salida")
+    private String salida;
+
+    @JsonProperty("StartTime")
+    private String startTime;
+
+    @JsonProperty("EndTime")
+    private String endTime;
+
+    @JsonProperty("IdCamera")
+    private String idCamera;
+}
