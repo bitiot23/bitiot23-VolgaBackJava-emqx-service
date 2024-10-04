342aa162100caf7d22bce82b4a8a3c1f9903d283
Cesar Munoz <cesarmu@hotmail.com>
2024-10-04 18:41:01 -0500

    Se configuró el controlador Rest CameraDataController para que recibiera los datos del topico Bit-iot/sanpedro/Neiva/Neiva/Camara/Vivotek/Viva Park que fue configurado en EMQX en un data brigde y las reglas

--- src/main/java/com/bitiot/volga3/emqx_to_rabbit/app/Controller/CameraDataController.java
+++ src/main/java/com/bitiot/volga3/emqx_to_rabbit/app/Controller/CameraDataController.java
@@ -2,6 +2,7 @@ package com.bitiot.volga3.emqx_to_rabbit.app.Controller;
 
 import com.bitiot.volga3.emqx_to_rabbit.app.service.CameraData;
 import com.bitiot.volga3.emqx_to_rabbit.app.service.CameraDataService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,23 +10,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+@Slf4j
 @RestController
 @RequestMapping("/guardar_datos_camara")
 public class CameraDataController {
 
     @PostMapping
-    public ResponseEntity<String> receiveCameraData(@RequestBody CameraData cameraData){
+    public ResponseEntity<String> receiveCameraData(@RequestBody CameraData payload){
         //Poner lógica para procesar los datos recibidos
         //Validar el payload y publicarlo en RabbitMQ
+        //Imprime los datos del payload
+        log.info("Datos recibidos del payload: {}", payload);
+
+        log.info("Report Time: {}", payload.getReportTime());
+
+        System.out.println("Report Time: " + payload.getReportTime());
+
         return ResponseEntity.ok("Datos recibidos y procesados");
     }
-
-//    @Autowired
-//    private CameraDataService cameraDataService;
-//
-//    @PostMapping("/fetch-camera-data")
-//    public String fetchCameraData() {
-//        // Llama al servicio que realiza la petición al webhook
-//        return cameraDataService.fetchCameraData();
-//    }
 }
