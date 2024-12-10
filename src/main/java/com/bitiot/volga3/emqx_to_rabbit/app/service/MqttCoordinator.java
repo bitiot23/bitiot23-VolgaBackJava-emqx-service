b96b4e5ce2dab359cebf02aab038d7c2c37992c5
Cesar Munoz <cesarmu@hotmail.com>
2024-12-10 08:38:26 -0500

    Se comentó un log en MqttCoordinator para limpiar consola de ejecución

--- src/main/java/com/bitiot/volga3/emqx_to_rabbit/app/service/MqttCoordinator.java
+++ src/main/java/com/bitiot/volga3/emqx_to_rabbit/app/service/MqttCoordinator.java
@@ -1,22 +1,23 @@
 package com.bitiot.volga3.emqx_to_rabbit.app.service;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class MqttCoordinator {// Coordina la lógica de suscripción para evitar las referencias circulares entre MqttService y MqttConnectionService
 
-    @Autowired
-    private MqttConnectionService mqttConnectionService;
+    private final MqttConnectionService mqttConnectionService;
+    private final MqttService mqttService;
 
-    @Autowired
-    private MqttService mqttService;
+    public MqttCoordinator(MqttConnectionService mqttConnectionService, MqttService mqttService) {
+        this.mqttConnectionService = mqttConnectionService;
+        this.mqttService = mqttService;
+    }
 
     @PostConstruct
     public void initialize(){
-        log.info("Ingresando al initialize");
+        //log.info("Ingresando al initialize");
         if (mqttConnectionService.connect()){ //Verifica si la conexión fue exitosa
             mqttService.subscribeToTopic(mqttConnectionService.getTopic());
 
